410c3e01615c362970adcbd7449ed58b0b83ad1e
its-amit-kumar <amitanku42@gmail.com>
2022-11-11 15:03:08 +0530

    added support for TLE

--- RunExecutable/RunExecutable.go
+++ RunExecutable/RunExecutable.go
@@ -4,19 +4,25 @@ import (
 	"os/exec"
 	"bytes"
 	"strings"
+	"context"
+	"time"
 )
 
 func Run(appAndArgument []string, length int, timelimit int, memorylimit int, input string)(string, string, error){
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
-	
-	cmd := exec.Command(appAndArgument[0], appAndArgument[1:]...)
+	timelimitConstrain, cancel := context.WithTimeout(context.Background(), time.Millisecond*time.Duration(timelimit*1000))
+	defer cancel()
+	cmd := exec.CommandContext(timelimitConstrain, appAndArgument[0], appAndArgument[1:]...)
 	cmd.Stdin = strings.NewReader(input)
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 	done := make(chan error, 1)
 	done <- cmd.Run()
-	err := <-done
-	return stdout.String(), stderr.String(), err
+	errTLE := <-done
+	if(errTLE!=nil){
+		return stdout.String(), "TLE", errTLE
+	}
+	return stdout.String(), stderr.String(), errTLE
 
 }
--- sample-files/file.cpp
+++ sample-files/file.cpp
@@ -9,6 +9,9 @@ int main()
 {
 	// prints hello world
 	cout << "Hello World";
+	while(true){
+
+	}
 
 	return 0;
 }
